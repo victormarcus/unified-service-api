f30e3cd003e8c750d5952ddb4c1b3b30045c7079
Abhishek Kumbhaj <kumbhaj.abhishek1289@gmail.com>
2022-11-21 22:10:19 +0530

    Fix "Connection pool shut down" issue, avoid using same client for all requests

--- src/main/java/com/unified/service/beeceptor/HttpPostClient.java
+++ src/main/java/com/unified/service/beeceptor/HttpPostClient.java
@@ -1,19 +1,11 @@
 package com.unified.service.beeceptor;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.unified.service.common.Constants;
 import com.unified.service.model.User;
@@ -21,14 +13,14 @@ import com.unified.service.utils.HttpUtils;
 import com.unified.service.utils.JsonUtils;
 
 public class HttpPostClient {
-
-	private static final CloseableHttpClient httpClient = HttpClients.createDefault();
-
+	
 	private static final ObjectMapper mapper = new ObjectMapper();
-
+	
 	public static boolean validateUser(User user, String protocol, String basePath, String endpointPath) throws Exception {
 		try {
 			String requestUrl = HttpUtils.generateRequestUrl(protocol, basePath, endpointPath);
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			
 			CloseableHttpResponse response = httpClient.execute(
 					HttpUtils.getPostRequest(Constants.APPLICATION_JSON, requestUrl, mapper.writeValueAsString(user)));
 			
